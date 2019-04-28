600bd7d184b1830766b5ad35234b7f7f8bd1ceaa
Pranitha Nagasai <46835976+PranithaNagasai@users.noreply.github.com>
2019-04-28 11:40:44 +0530

    Create sesion.js

---
+++ sesion.js
@@ -0,0 +1,12 @@
+var mongoose = require("mongoose"),
+passportLocalMongoose = require("passport-local-mongoose")
+
+var sesionschema= new mongoose.Schema({
+    username:String,
+    password:String,
+    role:String
+});
+
+sesionschema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model("sesion",sesionschema);
