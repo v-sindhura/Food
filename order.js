dcd03edf01e79fa7d5ae933661d3963ea5833ee5
Pranitha Nagasai <46835976+PranithaNagasai@users.noreply.github.com>
2019-04-28 11:39:58 +0530

    Create order.js

---
+++ order.js
@@ -0,0 +1,16 @@
+var mongoose=require("mongoose");
+
+var orderschema = new mongoose.Schema({
+	uname:{
+		type:mongoose.Schema.Types.ObjectId,
+		ref:"user"
+	},
+	o_id:Number,
+	o_date:String,
+	status:String,
+	remarks:String,
+	tot_cost:Number,
+	d_uname:{type:String,default:null}
+});
+
+module.exports = mongoose.model("order",orderschema);
