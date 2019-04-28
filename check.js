86e313c095c6e1b37ac84c2e28feaf5942e7b2cd
Pranitha Nagasai <46835976+PranithaNagasai@users.noreply.github.com>
2019-04-28 11:38:08 +0530

    Create check.js

---
+++ check.js
@@ -0,0 +1,11 @@
+var mongoose=require("mongoose");
+
+var checkschema = new mongoose.Schema({
+    o_id:Number,
+    item:Number,
+    status:String,
+    chef:String,
+    quantity:String
+})
+
+module.exports = mongoose.model("check",checkschema);
