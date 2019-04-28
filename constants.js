96e77a4cda3b3b9364da68b62eca6b42e1679cd5
Pranitha Nagasai <46835976+PranithaNagasai@users.noreply.github.com>
2019-04-28 11:50:05 +0530

    Create constants.js

---
+++ constants.js
@@ -0,0 +1,7 @@
+var check_hour=5;
+var check_minutes=30;
+var chef_success_msg="Order taken successfully";
+var chef_failure_msg="Order is already taken!!Kindly Refresh your page";
+var cook_success_msg="Item is marked successfully";
+var cook_failure_msg="Unable to mark!!!Kindly try again";
+module.exports=[check_hour,check_minutes]
