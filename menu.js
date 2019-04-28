304c0e3bb5891cfdfa9ba5197d87b18410b5f2c7
Pranitha Nagasai <46835976+PranithaNagasai@users.noreply.github.com>
2019-04-28 11:39:25 +0530

    Create menu.js

---
+++ menu.js
@@ -0,0 +1,12 @@
+var mongoose=require("mongoose");
+
+var menuschema = new mongoose.Schema({
+	item_id:Number,
+	item_name:String,
+	item_cost:Number,
+	item_desc:String,
+	item_url:String,
+	time_prepare:Number
+});
+
+module.exports = mongoose.model("menu",menuschema);
