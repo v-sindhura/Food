0af75adbec56028e52e867fada9dc80a12a9139f
Pranitha Nagasai <46835976+PranithaNagasai@users.noreply.github.com>
2019-04-28 11:39:01 +0530

    Create delivery.js

---
+++ delivery.js
@@ -0,0 +1,15 @@
+var mongoose = require("mongoose");
+passportLocalMongoose = require("passport-local-mongoose")
+
+var deliveryschema = new mongoose.Schema({
+
+    d_uname:String,
+    d_mobno:Number,
+    d_pwd:String
+
+});
+
+
+
+module.exports = mongoose.model("delivery",deliveryschema);
+
